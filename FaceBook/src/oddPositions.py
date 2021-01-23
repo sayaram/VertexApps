d7cfd877d337f347fbb3c127ecbbf852c56388e5
sayaram <sayaram@gmail.com>
2021-01-23 11:53:13 -0800

    Reformat

--- FaceBook/src/oddPositions.py
+++ FaceBook/src/oddPositions.py
@@ -5,6 +5,7 @@ def oddPositions(elem):
             pos.append(x)
     return pos
 
+
 if __name__ == '__main__':
     lst = [0, 1, 2, 3, 4, 5, 6, 7, 8]
     print(oddPositions(lst))
--- FaceBook/src/wordCount.py
+++ FaceBook/src/wordCount.py
@@ -1,14 +1,14 @@
-
 def wordCount(string):
     lst = string.split(" ")
     dic = {}
     for word in lst:
         if word in dic.keys():
-            dic[word] = dic[word] +1
+            dic[word] = dic[word] + 1
         else:
             dic[word] = 1
     for x in dic.items():
         print(x)
+
 
 if __name__ == '__main__':
     wordCount("Sayaram Gattu Mayukh Gattu Vishali Jangam")
